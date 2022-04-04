a2f205378fa12662638b1f3510e9cebab300d651
Ameer Hamza <aha1475@gmail.com>
2022-04-04 23:23:35 +0500

    Initial Commit

---
+++ Rugbysta/RugbystaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RugbystaApp.swift
+//  Rugbysta
+//
+//  Created by Ameer Hamza on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct RugbystaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
